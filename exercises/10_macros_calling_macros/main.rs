55b0554449e8fb6e9ab6fe857546f48b57a9f0a3
danielshih <dog830228@gmail.com>
2025-06-03 10:49:18 +0800

    finish exercises\10_macros_calling_macros\main.rs

--- exercises/10_macros_calling_macros/main.rs
+++ exercises/10_macros_calling_macros/main.rs
@@ -33,7 +33,11 @@ macro_rules! digit {
 
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
-// TODO: create `number!()` macro.
+macro_rules! number {
+    ($($num:ident )+) => {
+        concat!($(digit!($num)),+)
+    };
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
